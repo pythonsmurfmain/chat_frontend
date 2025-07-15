548c763cd1e7ac91db3b174b62274f768fc0a483
pythonsmurfmain <varil35414@gmail.com>
2025-07-15 22:39:18 +0530

    final

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -17,7 +17,7 @@ export default function Login({ onLogin }) {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center px-4" style={{backgroundColor:"#fb7185"}}>
+    <div className="min-h-screen bg-rose-500 flex items-center justify-center px-4">
       <motion.div
   initial={{ opacity: 0, y: 30 }}
   animate={{ opacity: 1, y: 0 }}
