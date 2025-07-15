68e323fe3b8540b1b28e5c925dc919d0606910c4
pythonsmurfmain <varil35414@gmail.com>
2025-07-15 21:01:48 +0530

    Fixed errors

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,5 +4,6 @@ const instance = axios.create({
   baseURL: "https://chat-backend-x2zf.onrender.com/", // Replace with your actual Render backend URL
   withCredentials: true,
 });
+axios.post('/api/messages', { text: message }, { withCredentials: true });
 
 export default instance;
